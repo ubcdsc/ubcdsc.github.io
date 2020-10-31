a15cd1a021ab2a5f4f317d9ddc617bc17cef0fb4
jrchan84 <justinrichardchan@gmail.com>
2020-10-31 10:07:31 -0700

    Updates

---
+++ precache-manifest.13e87db15bbcea383fee61cd88ba20d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a12069b266a1096a910a1749e62aa312",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29b77201cd38549118db",
+    "url": "/static/css/main.6daf450a.chunk.css"
+  },
+  {
+    "revision": "ee876c664faca9db95ab",
+    "url": "/static/js/2.672f18ee.chunk.js"
+  },
+  {
+    "revision": "7c0d33404c40478d39388afe86212e89",
+    "url": "/static/js/2.672f18ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29b77201cd38549118db",
+    "url": "/static/js/main.5b3ef179.chunk.js"
+  },
+  {
+    "revision": "6428853002f70d1acc3a",
+    "url": "/static/js/runtime-main.8412a077.js"
+  }
+]);
